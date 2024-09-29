b8dca9d421e4b8219aa0292155ef2f2f6bd74029
relax <3145849381@qq.com>
2024-09-29 14:53:12 +0800

    add some html code here

--- run.py
+++ run.py
@@ -3,17 +3,18 @@ from app import create_app
 
 
 app = create_app()
-@app.doc()
-@app.route('/')
+
+@app.route('/home')
 def home():
-    return render_template('index.html',title="下载页面")
+    return render_template('home.html',title="下载页面")
 
 
 @app.route('/download')
 def download():
     return render_template('download.html')
 
-
-
+@app.route('/faq')
+def faq():
+    return render_template('faq.html')
 if __name__ == '__main__':
     app.run(debug=True,port=6677)
