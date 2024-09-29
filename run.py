4ec82121f1f5677f882db740a72734f8648f393b
relax <3145849381@qq.com>
2024-09-29 23:04:27 +0800

    new version of home page PythonGuide page download page

--- run.py
+++ run.py
@@ -3,18 +3,22 @@ from app import create_app
 
 
 app = create_app()
-
+# 主页路由
 @app.route('/home')
 def home():
     return render_template('home.html',title="下载页面")
-
-
+# 下载页面路由
 @app.route('/download')
 def download():
     return render_template('download.html')
-
+# 安装过程可能会遇到的问题
 @app.route('/faq')
 def faq():
     return render_template('faq.html')
+# python的使用指南路由
+@app.route('/PythonGuide')
+def PythonGuide():
+    return render_template('AppGuide/PythonGuide.html')
+
 if __name__ == '__main__':
     app.run(debug=True,port=6677)
